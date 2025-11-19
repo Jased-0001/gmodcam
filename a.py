ddaef4dcbde5d73b70948f0afe224cc9d014fa19
Jase Williams <jased@jased.xyz>
2025-11-18 23:54:18 -0500

    make it work with websockets (really fast)

--- a.py
+++ a.py
@@ -1,16 +1,22 @@
 from flask import Flask, Response, request
+from flask_socketio import SocketIO, send, emit
 import cv2
 import atexit
 import time
+import base64
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
 camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
-width, height = 320, 240
-#width, height = 640, 480
-#width, height = 960, 720
+#width, height = 320,  240  # 240p (4:3)
+#width, height = 640,  480  # 480p (4:3)
+width, height = 960,  720  # 720p (4:3)
+#width, height = 1280, 1080 # 1080p (4:3)
 
 content_type, filetype = "image/jpg", ".jpg"
+compression = [int(cv2.IMWRITE_JPEG_QUALITY), 100]
 
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
@@ -25,32 +31,49 @@ def cleanup():
     camera.release()
 
 def generate_frame():
-    start = time.time()
+    #start = time.time()
     success, frame = camera.read()
     if not success:
         raise Exception("NOT SUCCESS " + success)
-    _, buffer = cv2.imencode(filetype, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
-    a = buffer.tobytes()
-    end = time.time()
-    print(f"Frametime: {end-start} - Size: {len(a)}")
-    return a
-def generate_frames():
-    while True:
-        yield (b'--frame\r\n'
-               b'Content-Type: '+ content_type.encode() + b' \r\n\r\n' + generate_frame() + b'\r\n')
+    _, buffer = cv2.imencode(filetype, frame, compression)
+    #end = time.time()
+    #print(f"Frametime: {end-start} - Size: {len(buffer)}")
+    return buffer
+def frameurl(frame):
+    return f'data:{content_type};base64,{base64.b64encode(frame).decode()}'
 
-@app.route('/video')
-def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/frame')
 def frame():
-    return Response(generate_frame(), mimetype=content_type)
+    return Response(generate_frame().tobytes(), mimetype=content_type)
+
+@app.route('/frame_url')
+def frameurl_():
+    return Response(frameurl(generate_frame()), mimetype="text/plain")
+
+
 
 @app.route('/')
 def page():
-    #with open("Untitled-1.html", "r") as f:
     with open("page.html", "r") as f:
-        return Response(f.read())
+        return Response(f.read(), mimetype='text/html')
+    
+@app.route('/video.js')
+def video():
+    with open("video.js", "r") as f:
+        return Response(f.read(), mimetype='text/javascript')
+    
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+@socketio.on('disconnect')
+def handle_connect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(data):
+    socketio.emit('frame', frameurl(generate_frame()))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, threaded=True)
+    #app.run(host='0.0.0.0', port=8080, threaded=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
---
+++ video.js
@@ -0,0 +1,110 @@
+var socket = io.connect('http://' + window.location.host);
+
+var scale = 1;
+
+function start() {
+    var canvas = document.getElementById("videoscreen");
+    var object = document.getElementById('videodata');
+    var status = document.getElementById("status");
+
+    var width = object.width * scale;
+    var height = object.height * scale;
+
+
+    canvas.setAttribute('width', width);
+    canvas.setAttribute('height', height);
+
+
+    var context;
+    if (canvas.getContext) {
+        context = canvas.getContext('2d', { willReadFrequently: true });
+    }
+
+
+    socket.on('connect', function () {
+        status.innerText = '';
+    });
+
+    socket.on('disconnect', function () {
+        status.innerHTML ='<span style="color:red;">HELP ME! HELP ME!</span>'
+    });
+
+    socket.on('frame', function (data) {
+        //console.log('Server says: ' + data);
+        object.src = data;
+
+        socket.send('frame');
+    });
+
+
+    //https://github.com/kasperific/HTML5ChromaKey
+    object.onload = function () {
+        context.drawImage(object, 0, 0, width, height);
+        imgDataNormal = context.getImageData(0, 0, width, height);
+        var imgData = context.createImageData(width, height);
+        for (i = 0; i < imgData.width * imgData.height * 4; i += 4) {
+            var r = imgDataNormal.data[i + 0];
+            var g = imgDataNormal.data[i + 1];
+            var b = imgDataNormal.data[i + 2];
+            var a = imgDataNormal.data[i + 3];
+            // set rgb levels for green and set alphachannel to 0;
+
+            // i have no idea how this works but #04f731 works for the chroma key
+            selectedR = 25;
+            selectedG = 250;
+            selectedB = 25;
+            if (r <= selectedR && g >= selectedG && b >= selectedB) {
+                a = 0;
+            }
+            if (a != 0) {
+                imgData.data[i + 0] = r;
+                imgData.data[i + 1] = g;
+                imgData.data[i + 2] = b;
+                imgData.data[i + 3] = a;
+            }
+        }
+        // For image anti-aliasing
+        //for (var y = 0; y < imgData.height; y++) {
+        //    for (var x = 0; x < imgData.width; x++) {
+        //        var r = imgData.data[((imgData.width * y) + x) * 4];
+        //        var g = imgData.data[((imgData.width * y) + x) * 4 + 1];
+        //        var b = imgData.data[((imgData.width * y) + x) * 4 + 2];
+        //        var a = imgData.data[((imgData.width * y) + x) * 4 + 3];
+        //        if (imgData.data[((imgData.width * y) + x) * 4 + 3] != 0) {
+        //            offsetYup = y - 1;
+        //            offsetYdown = y + 1;
+        //            offsetXleft = x - 1;
+        //            offsetxRight = x + 1;
+        //            var change = false;
+        //            if (offsetYup > 0) {
+        //                if (imgData.data[((imgData.width * (y - 1)) + (x)) * 4 + 3] == 0) {
+        //                    change = true;
+        //                }
+        //            }
+        //            if (offsetYdown < imgData.height) {
+        //                if (imgData.data[((imgData.width * (y + 1)) + (x)) * 4 + 3] == 0) {
+        //                    change = true;
+        //                }
+        //            }
+        //            if (offsetXleft > -1) {
+        //                if (imgData.data[((imgData.width * y) + (x - 1)) * 4 + 3] == 0) {
+        //                    change = true;
+        //                }
+        //            }
+        //            if (offsetxRight < imgData.width) {
+        //                if (imgData.data[((imgData.width * y) + (x + 1)) * 4 + 3] == 0) {
+        //                    change = true;
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        
+        context.putImageData(imgData, 0, 0);
+    }
+
+    socket.send('frame');
+}
+
+            
